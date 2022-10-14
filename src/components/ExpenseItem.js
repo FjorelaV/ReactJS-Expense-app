a09cc35cd762f4f9c5cb10e12ac568f96a380e2e
FjorelaV <fjorelaveliu1995@gmail.com>
2022-10-14 17:55:45 +0200

    feat(component): add first component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>October 14th 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$321.12</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
