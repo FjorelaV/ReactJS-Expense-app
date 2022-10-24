bedae4d2c7ab98997079cd5e753e324a60a993e5
FjorelaV <fjorelaveliu1995@gmail.com>
2022-10-24 16:48:01 +0200

    feat(Expense): form conditioning

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -68,6 +68,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add expense</button>
       </div>
     </form>
