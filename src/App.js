2e1d087e3ce77dde9e77d364980011510f2c313a
FjorelaV <fjorelaveliu1995@gmail.com>
2022-10-24 10:52:51 +0200

    feat(App): child-to-parent communication

--- src/App.js
+++ src/App.js
@@ -24,9 +24,13 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
